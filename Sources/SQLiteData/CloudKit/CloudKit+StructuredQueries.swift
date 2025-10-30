9c3bf82537bbce0345e170b89a8be95393a6fbf6
Stephen Celis <stephen@stephencelis.com>
2025-10-30 11:36:21 -0700

    Fix issues with `CK{Record,Share}.SystemFieldsRepresentation` (#284)
    * Fix issues with `CK{Record,Share}.SystemFieldsRepresentation`
    
    I'm a little surprised that this diff fixes things, but it does locally
    for me, in the latest Xcode 26, so let's test this change a bit before
    merging.
    
    * Compile time tests
    
    * wip;
    
    * wip
    
    ---------
    
    Co-authored-by: Brandon Williams <mbrandonw@hey.com>

--- Sources/SQLiteData/CloudKit/CloudKit+StructuredQueries.swift
+++ Sources/SQLiteData/CloudKit/CloudKit+StructuredQueries.swift
@@ -9,12 +9,12 @@
   #endif
   import StructuredQueriesCore
 
-  extension _CKRecord where Self == CKRecord {
+  extension CKRecord {
     public typealias _AllFieldsRepresentation = SQLiteData._AllFieldsRepresentation<CKRecord>
     public typealias SystemFieldsRepresentation = _SystemFieldsRepresentation<CKRecord>
   }
 
-  extension _CKRecord where Self == CKShare {
+  extension CKShare {
     public typealias _AllFieldsRepresentation = SQLiteData._AllFieldsRepresentation<CKShare>
     public typealias SystemFieldsRepresentation = _SystemFieldsRepresentation<CKShare>
   }
@@ -115,10 +115,6 @@
 
     private struct DecodingError: Error {}
   }
-
-  extension CKRecord: _CKRecord {}
-
-  public protocol _CKRecord {}
 
   extension CKDatabase.Scope {
     public struct RawValueRepresentation: QueryBindable, QueryRepresentable {
